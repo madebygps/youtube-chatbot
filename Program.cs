80c26ef23695faab6d12d166f6563acfe7a848df
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-04-05 21:38:43 -0400

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Common;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var youtube = new YoutubeClient();
+        var channelUrl = "https://www.youtube.com/channel/UCbjgKwnWnGG7sKCPTRgrFcw";
+
+        var videos = await youtube.Channels.GetUploadsAsync(channelUrl);
+
+        foreach (var video in videos)
+        {
+            var videoInfo = await youtube.Videos.GetAsync(video.Id);
+            Console.WriteLine($"{videoInfo.Title} - {videoInfo.Author} - {videoInfo.Id}");
+        }
+    }
+}
